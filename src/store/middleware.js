2107bce3535e585d98fc2281b913b90d3729cc15
Sylvain Laugier <sylvain.laugier.net@gmail.com>
2020-02-20 11:17:14 +0100

    better middlware order and check mate checking

--- src/store/middleware.js
+++ src/store/middleware.js
@@ -1,12 +1,16 @@
 import ActionTypes from '../actions/types';
-import { unselectPiece, capturePiece, switchTurn } from '../actions/actions';
-import { getPieceIdOnSquareByPosition } from '../reducers/selectors';
+import {
+  unselectPiece,
+  capturePiece,
+  switchTurn,
+  setWonGame,
+} from '../actions/actions';
+import { getPieceIdOnSquareByPosition, getTurn } from '../reducers/selectors';
 import { List } from 'immutable';
+import { isKingCheckedMate } from '../chess-rules/chessRules';
 
 const middleware = store => next => action => {
   if (action.type === ActionTypes.MOVE_PIECE) {
-    store.dispatch(unselectPiece());
-    store.dispatch(switchTurn());
     const { i, j } = action.payload;
     const capturedPiece = getPieceIdOnSquareByPosition(
       store.getState(),
@@ -15,6 +19,15 @@ const middleware = store => next => action => {
     if (capturedPiece) {
       store.dispatch(capturePiece(capturedPiece));
     }
+    next(action);
+    store.dispatch(unselectPiece());
+    store.dispatch(switchTurn());
+    const newState = store.getState();
+    if (isKingCheckedMate(newState, getTurn(newState))) {
+      store.dispatch(setWonGame());
+    }
+
+    return;
   }
   return next(action);
 };
